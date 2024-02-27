47b8d659beaadfa6c2174c54b95d504023ce4c0f
danielfatec2006 <daniel.silva397@fatec.sp.gov.br>
2024-02-27 12:36:32 -0300

    Aula2702 - Componentes do React

---
+++ basic/basictools/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import 'react'
+
+const NavBar = () => {
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                <a href="#">Institucional</a>
+                </li>
+                <li>
+                <a href="#">Sobre</a>
+                </li>
+                <li>
+                <a href="#">Notícas</a>
+                </li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
